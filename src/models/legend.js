363d55a290935e071ce4892373a7b802b14b3f00
yang.liuy <yang.liuy@alibaba-inc.com>
2016-03-31 21:29:53 +0800

    reset legend

--- src/models/legend.js
+++ src/models/legend.js
@@ -112,12 +112,11 @@ nv.models.legend = function() {
                             }
                             else {
                                 d.disabled = !d.disabled;
-                                // 允许全部隐藏
-                                // if (data.every(function(series) { return series.disabled})) {
-                                //     //the default behavior of NVD3 legends is, if every single series
-                                //     // is disabled, turn all series' back on.
-                                //     data.forEach(function(series) { series.disabled = false});
-                                // }
+                                if (data.every(function(series) { return series.disabled})) {
+                                    //the default behavior of NVD3 legends is, if every single series
+                                    // is disabled, turn all series' back on.
+                                    data.forEach(function(series) { series.disabled = false});
+                                }
                             }
                         } else if(vers == 'furious') {
                             if(expanded) {
